37b179289a416d48249ff3be1b4f78109a00f76f
Gijs van Zoelen <gijsvanzoelen@outlook.com>
2024-10-29 11:55:08 +0100

    Update nightly_build.sh

--- nightly_build.sh
+++ nightly_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-docker-compose build
+docker-compose -f docker-compose-build.yml build
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 093215643118.dkr.ecr.us-east-1.amazonaws.com
 docker-compose push
-docker stack deploy -c docker-compose.yml eshop-stack
+docker stack deploy -c docker-compose-deploy.yml eshop-stack
